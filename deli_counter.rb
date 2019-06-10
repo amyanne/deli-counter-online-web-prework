76247a1b2905620d6accc6e5e616c691327d3eea
Amy Westcott <amy.westcott@yahoo.com>
2019-06-10 00:33:47 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -20,6 +20,7 @@ def take_a_number(array, name)
     counter += 1 
     complete_array.push("Welcome, #{name}. You are number #{counter} in line.")
      end
+     complete_array.uq
      puts complete_array
     end
     
