3efc328bb064be95b7de1fec9e2070ed81977a01
Amy Westcott <amy.westcott@yahoo.com>
2019-06-10 00:19:17 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,24 @@
-# Write your code here.
+require "pry"
+def line(array)
+  if array.length > 0 
+    puts "The line is currently: "
+    array.each_with_index do |index, name|
+      index += 1 
+      puts "#{index}. #{name}"
+      end
+  else 
+    puts "The line is currently empty."
+  end 
+end
+def take_a_number(array, name)
+      array.push(name)
+  counter = 0
+  array.each do |name|
+    counter += 1 
+    puts "Welcome, #{name}. You are number #{counter} in line."
+    
+    
+  end
+    end
+    
+    
